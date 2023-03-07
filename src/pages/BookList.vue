b31e7bd69982b5cc6dc4facbf9e920cf533aa3b8
cerry24 <samuelecerretti@gmail.com>
2023-03-07 16:17:34 +0100

    implement Api call to get books from back-end

---
+++ src/pages/BookList.vue
@@ -0,0 +1,46 @@
+<script>
+import axios from 'axios';
+import { store } from '../store';
+
+export default {
+    name: 'BookList',
+
+    components: {
+
+    },
+
+    data() {
+        return {
+            store,
+            apiAddress: '127.0.0.1:8000'
+        }
+    },
+
+    methods: {
+        getBooks() {
+            axios.get(this.apiAddress + '/api/books/', {
+            params: {
+            }
+        })
+        .then((response) => {
+            console.log(response.data.results.data);
+            store.books = response.data.results.data;
+        })
+        .catch(function (error) {
+            console.warn(error);
+        });
+        }
+    },
+
+    created() {
+        this.getBooks();
+    },
+}
+</script>
+
+<template>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
