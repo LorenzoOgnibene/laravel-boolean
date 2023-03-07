188e667c72d733a2174882cd71c6ce113e158597
LorenzoOgnibene <lorenzo.ognibene@gmail.com>
2023-03-07 16:33:44 +0100

    imported router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import { createApp } from 'vue'
-<<<<<<< HEAD
-=======
 import { router } from './router'
->>>>>>> ba7881832d54b9f684a3883d381eabc370f0f55e
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
